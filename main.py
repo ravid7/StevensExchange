034dd7a7f8b427150c49afff6ef17dbab2d32f41
ravid7 <rrathor2@stevens.edu>
2020-05-02 16:45:35 -0400

    html edited

--- main.py
+++ main.py
@@ -19,14 +19,34 @@ from flask import Flask, Markup, render_template
 app = Flask(__name__)
 
 top_labels = [
-    "MSFT", "AAPL", "UBER", "GPRO", 
+    "MSFT", "AAPL", "UBER", "GPRO",
     "LYFT", "TWTR", "NFLX", "TSLA"
 ]
 
+color_strings = [
+    "list-group-item-primary",
+    "list-group-item-secondary",
+    "list-group-item-success",
+    "list-group-item-danger",
+    "list-group-item-warning",
+    "list-group-item-info",
+    "list-group-item-light",
+    "list-group-item-dark"
+]
+
+
+def live_price_editor():
+    for i in range(len(top_labels)):
+        top_labels[i] += " (%.2f)" % get_live_price(top_labels[i])
+
+
 @app.route('/')
 def main():
-    return render_template('main_page.html', title="StevensEx Stock monitor")
+    return render_template('main_page.html', title="StevensEx Stock monitor", labels=zip(color_strings, top_labels))
+
+# print(top_labels)
 
 
 if __name__ == '__main__':
-    app.run('127.0.0.1', 5555, debug=True)
+    live_price_editor()
+    app.run('127.0.0.1', 8080, debug=True)
--- valid.py
+++ valid.py
@@ -1,7 +1,7 @@
-from yahoo_fin.stock_info import *
-from yahoo_fin.options import *
-from datetime import datetime
+# from yahoo_fin.stock_info import *
+# from yahoo_fin.options import *
+# from datetime import datetime
 
-current_date = datetime.today().strftime('%m/%d/%Y')
-# print(get_calls('nflx', str(current_date)))
-print(get_calls('nflx', "04/01/2020"))
+# current_date = datetime.today().strftime('%m/%d/%Y')
+# data = get_data('gold', start_date='04/30/2020', end_date='05/02/2020')
+# print(data.open, data.close)
