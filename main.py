1c353ff98251610d581a73d2be162f9ceb4979bc
ravid7 <rrathor2@stevens.edu>
2020-05-03 20:04:36 -0400

    login_register page

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 '''
 **************************************
-**************
-***************
-rrathor2@stevens.edu, ravid7.github.io
-***************
-**************
+
+<<<<<<<<<<<<<<<<<<<<<<<<<<|>>>>>>>>>>>>>>>>>>>>>>>>>>>
+<<<<<<<<<<<<<<<<<<<<<<<<<   >>>>>>>>>>>>>>>>>>>>>>>>>>
+<<<<<<<<<<<<<<<<<<                  >>>>>>>>>>>>>>>>>>
+<<<<<<<<<<<<<<      -------------      >>>>>>>>>>>>>>
+<<<<<<< rrathor2@stevens.edu, ravid7.github.io >>>>>>>
+<<<<<<<<<<<<<<                          >>>>>>>>>>>>>>
+<<<<<<<<<<<<<<<<<<                  >>>>>>>>>>>>>>>>>>
+<<<<<<<<<<<<<<<<<<<<<<<<<   >>>>>>>>>>>>>>>>>>>>>>>>>>
+<<<<<<<<<<<<<<<<<<<<<<<<<<|>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
 **************************************
 '''
 
@@ -13,9 +19,24 @@ from yahoo_fin.options import *
 import pandas as pd
 import sqlite3
 from sqlite3 import Error
-from flask import Flask, Markup, render_template
+from flask import Flask, Markup, render_template, url_for, flash, \
+    redirect
+from signin_login import RegisterationForm, LoginForm
 
 app = Flask(__name__)
+
+labels = [
+    'JAN', 'FEB', 'MAR', 'APR',
+    'MAY', 'JUN', 'JUL', 'AUG',
+    'SEP', 'OCT', 'NOV', 'DEC'
+]
+
+values = [
+    967.67, 1190.89, 1079.75, 1349.19,
+    2328.91, 2504.28, 2873.83, 4764.87,
+    4349.29, 6458.30, 9907, 16297
+]
+
 
 top_labels = [
     "MSFT", "AAPL", "UBER", "GPRO",
@@ -38,17 +59,40 @@ color_strings = [
 
 def live_price_editor():
     for i in range(len(top_labels)):
-        final_label.append(" (%.2f)" % get_live_price(top_labels[i]))
+        final_label.append(top_labels[i] + " (%.2f)" % get_live_price(top_labels[i]))
 
 
 @app.route('/')
 def main():
+    line_labels = labels
+    line_values = values
     return render_template('main_page.html', title="StevensEx Stock monitor", \
-         labels=zip(color_strings, final_label))
+         top_labels=zip(color_strings, final_label), max=17000, labels=line_labels, values=line_values)
 
 # print(top_labels)
 
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    register = RegisterationForm()
+    if register.validate_on_submit():
+        flash(f'Account created for {register.username.data}!', 'success')
+        return redirect(url_for('main'))
+    return render_template('register.html', title="Sign up", form=register)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    login = LoginForm()
+    if login.validate_on_submit():
+        # flash(f'')
+        if login.email.data == 'xxx@gmail.com':
+            flash('You are logged in', 'success')
+            return redirect(url_for('main'))
+        else:
+            flash('Make sure you enter your credentials correctly! Failed to login.', 'danger')
+    return render_template('login.html', title="Sign in", form=login)
 
 if __name__ == '__main__':
     live_price_editor()
+    app.config['SECRET_KEY'] = 'ef7d72b3b1ec8f38952eb95b9bb6b6f1'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://site.db'
     app.run('127.0.0.1', 8080, debug=True)
