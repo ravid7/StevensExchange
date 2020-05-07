5d09be7f773da2dbdb21f0eada86306095ad770b
Sachin Ruhil <sachinruhil@Sachins-MacBook-Pro.local>
2020-05-07 14:28:36 -0700

    ..

--- StevenEx/TestQuery.py
+++ StevenEx/TestQuery.py
@@ -1,1 +1,28 @@
 import sqlite3
+
+class Inject:
+
+    def __init__(self, temp=False):
+        self.connection = None
+        if temp:
+            self.connection = sqlite3.connect(":memory:")
+        else:
+            self.connection = sqlite3.connect("./databases/mem.db")
+        self.cursor = self.connection.cursor()
+
+    def create(self, query):
+        self.cursor.execute(query)
+        self.finalize()
+
+    def insert(self, table, *items):
+        result = self.cursor.execute("INSERT INTO {} VALUES {}".format(table, items))
+        self.finalize()
+        return result
+
+
+
+    def finalize(self):
+        self.connection.commit()
+
+    def close(self):
+        self.connection.close()
