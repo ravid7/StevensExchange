3157d4df7ffc131b47270f913fd04775877f49bf
Sachin Ruhil <sachinruhil@Sachins-MacBook-Pro.local>
2020-05-07 14:26:49 -0700

    ok-tested

--- StevenEx/forms.py
+++ StevenEx/forms.py
@@ -3,23 +3,29 @@ from wtforms import StringField, PasswordField, SubmitField, BooleanField, Selec
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from StevenEx.models import User
 
+#Registering the user
 class RegisterationForm(FlaskForm):
+    #defining validators for Username, email, password,confirm password- 
+    # we have to make sure we don't forget Equal to in confirm pass
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
+    # equal to checklist - Done( FOI )
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), 
                                             EqualTo('password')])
     submit = SubmitField('Sign Up')
+
+    #validating user name raise error if already exist
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user:
             raise ValidationError('That username is not available. Please choose a different one.')
-
+    #validating email rasie error if already exist
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user:
             raise ValidationError('That email is already taken. Please choose a different one.')
-
+# Login form ( validator - Data must be required)
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
